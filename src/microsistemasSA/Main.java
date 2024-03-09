fe8e29910854ebf521ad0a2cb40bf24f11e19701
Jonathan Ramiro Elías Vásquez <99297546+jonathanrev2003@users.noreply.github.com>
2024-03-09 17:55:10 -0600

    initial comit

--- src/microsistemasSA/Main.java
+++ src/microsistemasSA/Main.java
@@ -1,38 +1,73 @@
 package microsistemasSA;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Lista_Metodos a = new Lista_Metodos();
+        Scanner scanner = new Scanner(System.in);
 
         /***
+         *
          * El metodo implementado para manejar las solicitudes de manera local es
-         * lista enlasada simple para manejar una solicitud a la vez.
+         * lista enlasada simple para manejar una solicitud a la vez (solicitud que entra solicitud que se resuelve).
+         *
          */
 
-        Lista_Metodos a = new Lista_Metodos();
+        //simulating multiple user requests.
+        a.insertarInicio("Solicitud NO.4");
+        a.insertarInicio("Solicitud NO.3");
+        a.insertarInicio("Solicitud NO.2");
+        a.insertarInicio("Solicitud NO.1");
 
-        a.insertarFinal("A");
-        a.insertarFinal("B");
-        a.insertarFinal("C");
-        a.insertarFinal("D");
-        a.insertarFinal("E");
-        a.insertarInicio("Inicio");
 
-        System.out.println("Lista completa");
-        a.mostrarLista();
+        //interface / menu
+        System.out.println("\n   Bienbenido al sistema de <<MICRO SISTEMAS S.A.>>   ");
+        System.out.println(" Que desea hacer hoy escriba el numero de la accion \n");
+        int seleccion = 0;
 
-        System.out.println();
-        System.out.println("Eliminamos el inicio");
-        a.eliminarInicio();
-        a.mostrarLista();
+        do {
+            System.out.println("\n 1. Agregar nueva solicitiud al final de solicitudes ");
+            System.out.println(" 2. Consultar todas las solicitudes ");
+            System.out.println(" 3. eliminar primera solicitud de entrada ");
+            System.out.println(" 4. ver el inicio de solicitudes");
+            System.out.println(" 5. salir");
 
-        System.out.println();
-        System.out.println("Extraemos el nuevo inicio");
+            seleccion = scanner.nextInt();
 
-        System.out.println(a.extraerElInicio());
-        System.out.println();
-        System.out.println("Nueva lista completa");
+            switch (seleccion){
+                case 1:
 
-        a.mostrarLista();
+                    System.out.println("Escriba la solicitud a agreagrar\n");
+                    Scanner soli = new Scanner(System.in);
+                    String solicitud = soli.nextLine();
+                    a.insertarFinal(solicitud);
+                    System.out.println("Solicitud agregada\n");
+                    break;
 
+                case 2:
+
+                    System.out.println("Estas son todas las solicitiudes\n");
+                    a.mostrarLista();
+                    break;
+
+                case 3:
+                    System.out.println("Solicitud eliminada...\n");
+                    a.eliminarInicio();
+                    break;
+
+                case 4:
+                    System.out.println("Este es el inicio de solicitudes\n");
+                    a.extraerElInicio();
+                    break;
+
+                case 5:
+                    break;
+
+            }
+        }while (seleccion != 5);
+        System.out.println("PROGRAMA FINALISADO");
     }
 }
+
+
