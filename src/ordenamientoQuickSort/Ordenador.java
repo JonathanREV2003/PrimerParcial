2eb33513d5bde973a5116d8fe38655f0f2a45490
Jonathan Ramiro Elías Vásquez <99297546+JonathanREV2003@users.noreply.github.com>
2024-03-09 17:55:10 -0600

    initial comit

--- src/ordenamientoQuickSort/Ordenador.java
+++ src/ordenamientoQuickSort/Ordenador.java
@@ -13,9 +13,25 @@ public class Ordenador {
         int i = izquierda, d = dercha;
         if (izquierda!=dercha){
             int pivote;
-
+            int aux;
+            pivote = izquierda;
+            while (izquierda!=dercha){
+                while (numeros[dercha] >= numeros[pivote] && izquierda < dercha)
+                    dercha--;
+                while (numeros[izquierda] < numeros [pivote] && izquierda < dercha)
+                    izquierda++;
+                if(dercha != izquierda){
+                    aux = numeros[dercha];
+                    numeros[dercha] = numeros[izquierda];
+                    numeros[izquierda] = aux; }
+                }
+            if (izquierda == dercha){
+                quicksort2(numeros,i,izquierda-1);
+                quicksort2(numeros,izquierda+1,d);
+            }
+            }
+        else
+            return numeros;
+        return numeros;
         }
-
-
     }
-}
